430cdb845c24c597bfb989361cc105ab4a2e6126
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-09-24 13:37:25 +0200

    24-09-20 V0

--- PHP/pendu.php
+++ PHP/pendu.php
@@ -10,18 +10,18 @@ afficherTableau($tab);*/
 echo "Cette méthode doit donner        et ca donne : " ;
 afficherTableau(coderMot($test));*/
 
-Echo "Cette méthode doit donner \n 1 \n 4 et ca donne \n" ;
-$t = array( 'B', 'O', 'N', 'J', 'O', 'U', 'O' );
+/*Echo "Cette méthode doit donner \n 1 \n 4 et ca donne \n" ;
+$t = array( 'B', 'O','O','N','J', 'O', 'U', 'R');
 $positions = testerLettre('O', $t,0);
 foreach ($positions as $pos)
 {
     echo("position : ".$pos."\n");
-}
+}*/
 
 /*Echo "Cette méthode doit donner B O N K O U R et ca donne ";
 $t = array( 'B', 'O', 'N', 'J', 'O', 'U', 'R' );
-afficherTableau( ajouterUneLettre('D', $t, 0));*/
-
+afficherTableau( ajouterUneLettre('K', $t, 3));
+*/
 /*$motATrouver="BONJOUR";
 $t = array( 'B', '_', 'N', 'J', '_', 'U', '_' );
 echo "Cette méthode doit donner B O N J O U _ et ca donne ";
@@ -29,4 +29,10 @@ afficherTableau(ajouterLesLettres('O', $t, testerLettre('O', str_split($motATrou
 // en l’absence des autres methodes
 //Print_r(ajouterLesLettres('O', $t,[1,4]);*/
 
+/*$liste = array('A','B','C') ;
+echo "Cette méthode doit donner :\n Les lettres non présentes sont A,B,C \n et ca donne \n" ;
+afficherMauvaisesLettres($liste);*/
+
+
 //DessinerPendu(5);
+
