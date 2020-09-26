665005d903dc2113fecb35b1a4306a310adb5607
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-09-26 19:51:38 +0200

    26-09-20 v0

--- PHP/exercice4-8.php
+++ PHP/exercice4-8.php
@@ -1,4 +1,6 @@
 <?php
+
+// Saisie des données de la date 
 do{   
    $jour=readline("Entrez le jour 'jj' : ");
     if (ctype_digit($jour)!=1)
@@ -21,22 +23,22 @@ do{
      }
  }while(ctype_digit($annee)!=1);
 
-if ((($annee>0)&&(($jour>0))&&(($jour<=31))&&(($mois>0))&&($mois<=12)))
+if ((($annee>0)&&(($jour>0))&&(($jour<=31))&&(($mois>0))&&($mois<=12))) // si année >0 0<jours<=31 et 0<mois<=12
 {
-    if ((($mois==4)||($mois==6)||($mois==9)||($mois==11))&&($jour>30))
+    if ((($mois==4)||($mois==6)||($mois==9)||($mois==11))&&($jour>30)) // si mois de 30 jours
     {
         $date="ko";
     }
     else
     {
-        if ($mois==2){
+        if ($mois==2){   // si mois de février
             if ($jour<=28)
             {
                 $date="ok";
             }
             else 
             {
-                if (($jour==29)&&(($annee%400==0)||(($annee%4==0)&&($annee%100!=0))))
+                if (($jour==29)&&(($annee%400==0)||(($annee%4==0)&&($annee%100!=0)))) //si février avec 29 jours test si année bissextile
                 {
                             $date="ok";
                 }
@@ -57,6 +59,8 @@ if ((($annee>0)&&(($jour>0))&&(($jour<=31))&&(($mois>0))&&($mois<=12)))
 else {
     $date="ko";
 }
+
+// Affichage du resultat
 if($date=="ok")
 {
     echo"La date ".$jour."-".$mois."-".$annee." existe.";
