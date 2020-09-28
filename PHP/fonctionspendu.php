e5db7a91f55df523d46a6c22c06fcb4c5b829abe
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-09-28 21:58:00 +0200

    28-09-2020 v8

--- PHP/fonctionspendu.php
+++ PHP/fonctionspendu.php
@@ -98,13 +98,13 @@ function ajouterUneLettre($lettre,$tab,$position)
  */
 function ajouterLesLettres($lettre,$tab,$listePosition)
 {
-    /*
+    
     // affiche les lettres doubles en meme temps
     foreach($listePosition as $index)
     {
         $tab=ajouterUneLettre($lettre,$tab,$index);
-    }*/
-    $positionProvi=array();
+    }
+    /*$positionProvi=array();
     foreach ($listePosition as $index)
     {
         if($tab[$index]=="_")
@@ -114,7 +114,7 @@ function ajouterLesLettres($lettre,$tab,$listePosition)
         
     }
     $alea=array_rand($positionProvi);
-    $tab=ajouterUneLettre($lettre,$tab,$positionProvi[$alea]);
+    $tab=ajouterUneLettre($lettre,$tab,$positionProvi[$alea]);*/
     return $tab;
 }
 
@@ -1072,7 +1072,86 @@ function creer_dico()
     return $tabMots;
 }
 
-$tab=array("_","O","_","_","O","_","O");
-$position=array(1,3,4,6);
-$tab=ajouterLesLettres("O",$tab,$position);
-var_dump($tab);
+/**
+ * Méthode qui lance et gère une partie
+ */
+function lancerPartie()
+{
+do{
+    $nberreurs=0;        
+    $mauvaiseslettres=array();
+    $lettresaisie=array();
+    $nberreurmax=8;
+
+    // selection et codage automatique du mot 
+    $motAt=choisirMot();
+    echo $motAt."\n";
+
+    
+    
+    // conversion du mot en tableau 
+    $tabmotcode = str_split($motAt);
+
+    // selection du niveau de difficulté 1 ou 2
+    do{
+        $difficulte=readline("Entrez un niveau de difficulté 1 ou 2 : ");
+    }while(($difficulte!=1)&&($difficulte!=2));
+    
+    $motcode=coderMot($motAt,$difficulte);
+    
+    do
+    {
+        dessinerPendu($nberreurs);
+        afficherTableau($motcode);
+        echo"\n Il vous reste ".(8-$nberreurs)." chances.\n";
+
+        // affichage de la liste de mauvaises lettres si le joueur à donné des mauvaises lettres
+
+        if(!empty($mauvaiseslettres))
+        {
+            afficherMauvaisesLettres($mauvaiseslettres);
+        }
+        
+        $lettre=demanderLettre($lettresaisie);
+        $lettresaisie[]=$lettre;
+        $position=testerLettre($lettre,$tabmotcode,0);
+
+        if($position!=[])  // Si la lettre se trouve dans le mot
+        {
+            $motcode=ajouterLesLettres($lettre,$motcode,$position);
+           
+        }
+        else
+        {
+            if(!in_array($lettre,$mauvaiseslettres))//Si la mauvaise lettre n'a pas déjà été donnée 
+            {
+                $mauvaiseslettres[]=$lettre;
+            }
+            $nberreurs++;
+        }
+        $gagne=testerGagner($nberreurs,$motcode);   
+    }while($gagne==0);
+
+    // affichage du mot du pendu et du résultat de la partie 
+    dessinerPendu($nberreurs);
+    //afficherTableau($tabmotcode);
+
+    if($gagne==1)
+    {
+        echo "\n******* La partie est gagnée *******\n\n";
+        echo "Le mot été $motAt";
+    }
+    else
+    {
+        echo "\n******* La partie est perdue !!!!!! *******\n\n";
+        echo "Le mot été $motAt"; 
+    }
+    echo "\n\n";
+
+    // demande à l'utilisateur si il veux rejouer
+    do{
+        $rejouer=strtolower(readline ("voulez vous rejouer (O/N) ? "));
+    }while(($rejouer!="o")&&($rejouer!="n"));
+
+}while($rejouer=="o");
+}
--- PHP/pendu.php
+++ PHP/pendu.php
@@ -1,84 +1,8 @@
 <?php
 require "fonctionspendu.php";
-function lancerPartie()
-{
-do{
-    $nberreurs=0;        
-    $mauvaiseslettres=array();
-    $lettresaisie=array();
-    $nberreurmax=8;
 
-    // selection et codage automatique du mot 
-    $motAt=choisirMot();
-    echo $motAt."\n";
 
-    
-    
-    // conversion du mot en tableau 
-    $tabmotcode = str_split($motAt);
 
-    // selection du niveau de difficulté 1 ou 2
-    do{
-        $difficulte=readline("Entrez un niveau de difficulté 1 ou 2 : ");
-    }while(($difficulte!=1)&&($difficulte!=2));
-    
-    $motcode=coderMot($motAt,$difficulte);
-    
-    do
-    {
-        dessinerPendu($nberreurs);
-        afficherTableau($motcode);
-        echo"\n Il vous reste ".(8-$nberreurs)." chances.\n";
 
-        // affichage de la liste de mauvaises lettres si le joueur à donné des mauvaises lettres
-
-        if(!empty($mauvaiseslettres))
-        {
-            afficherMauvaisesLettres($mauvaiseslettres);
-        }
-        
-        $lettre=demanderLettre($lettresaisie);
-        //$lettresaisie[]=$lettre;
-        $position=testerLettre($lettre,$tabmotcode,0);
-
-        if($position!=[])  // Si la lettre se trouve dans le mot
-        {
-            $motcode=ajouterLesLettres($lettre,$motcode,$position);
-           
-        }
-        else
-        {
-            if(!in_array($lettre,$mauvaiseslettres))//Si la mauvaise lettre n'a pas déjà été donnée 
-            {
-                $mauvaiseslettres[]=$lettre;
-            }
-            $nberreurs++;
-        }
-        $gagne=testerGagner($nberreurs,$motcode);   
-    }while($gagne==0);
-
-    // affichage du mot du pendu et du résultat de la partie 
-    dessinerPendu($nberreurs);
-    //afficherTableau($tabmotcode);
-
-    if($gagne==1)
-    {
-        echo "\n******* La partie est gagnée *******\n\n";
-        echo "Le mot été $motAt";
-    }
-    else
-    {
-        echo "\n******* La partie est perdue !!!!!! *******\n\n";
-        echo "Le mot été $motAt"; 
-    }
-    echo "\n\n";
-
-    // demande à l'utilisateur si il veux rejouer
-    do{
-        $rejouer=strtolower(readline ("voulez vous rejouer (O/N) ? "));
-    }while(($rejouer!="o")&&($rejouer!="n"));
-
-}while($rejouer=="o");
-}
 
 lancerPartie();
