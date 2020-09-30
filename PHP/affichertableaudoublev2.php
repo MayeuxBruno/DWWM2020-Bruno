24336eacb83823ee00ddc71a35ea46177f8ba91d
59011-95-09 <59011-95-09@exchange.ad.afpanet>
2020-09-30 08:49:21 +0200

    30-09

--- PHP/affichertableaudoublev2.php
+++ PHP/affichertableaudoublev2.php
@@ -2,35 +2,35 @@
 $t[0]=[0,1,2,4,0];
 $t[1]=[3,4,5,4,1];
 $t[2]=[6,7,8,9,2];
-$t[3]=[1,2,1,3,4];
+$t[3]=[1,20,1,3,4];
+$t[4]=
 
-echo "\t    ";
+echo "\t\t\t";
 for($l=0;$l<count($t[0]);$l++)
 {
-    echo ($l+1)."   ";
+    echo ($l+1)."\t\t";
 }
 echo "\n";
 
 for($i=0;$i<count($t);$i++)
 {
-    echo "\t   ";
+    echo "\t\t";
     for($l=0;$l<count($t[0]);$l++)
     {
-        echo "----";
+        echo "----------------";
     }
     echo "\n";
     echo "\t".($i+1);
     for($j=0;$j<count($t[$i]);$j++)
     {
-        echo " | ".$t[$i][$j];
+        echo "\t|\t".$t[$i][$j];
     }
-    echo" |";
+    echo"\t|";
     echo "\n";
 }
-echo "\t   ";
+echo "\t\t";
 for($l=0;$l<count($t[0]);$l++)
     {
-        echo "----";
+        echo "----------------";
     }
     echo "\n";
-
