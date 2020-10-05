db6134b54622b4c5bf94e2c875cfee60594ae5d6
59011-95-09 <59011-95-09@exchange.ad.afpanet>
2020-10-05 09:51:52 +0200

    05-10-2020 v1

--- PHP/objet/test.php
+++ PHP/objet/test.php
@@ -3,4 +3,14 @@
 Class test 
 {
     private $_nom;
+
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+
+    public function setNom($_nom)
+    {
+        $this->_nom = $_nom;
+    }
 }
