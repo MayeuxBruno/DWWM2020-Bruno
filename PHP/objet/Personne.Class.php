007e99391fb861a003ec828684a04653b164c17c
59011-95-09 <59011-95-09@exchange.ad.afpanet>
2020-10-05 10:01:47 +0200

    05-10-2020 v3

--- PHP/objet/Personne.Class.php
+++ PHP/objet/Personne.Class.php
@@ -11,10 +11,10 @@ Class Personne
     //Assesseurs
     public function __construct($nom,$prenom,$age,$genre)
     {
-        $this->setNom=$nom;
-        $this->setPrenom=$prenom;
-        $this->setAge=$age;
-        $this->setGenre=$genre;
+        $this->setNom($nom);
+        $this->setPrenom($prenom);
+        $this->setAge($age);
+        $this->setGenre($genre);
     }
 
     //Getters
@@ -39,26 +39,66 @@ Class Personne
     }
 
     //Setters
-    public function setNom($nom)
+    public function setNom(String $nom)
     {
-        $this->_nom=$nom;
+        $this->_nom=strtoupper($nom);
     }
 
-    public function setPrenom($prenom)
+    public function setPrenom(String $prenom)
     {
-        $this->_prenom=$prenom;
+        $this->_prenom=strtoupper($prenom);
     }
 
-    public function setAge($age)
+    public function setAge(Int $age)
     {
-        $this->_age=$age;
+        $this->_age=$age>0 ? $age : null;
     }
 
-    public function setGenre($genre)
+    public function setGenre(String $genre)
     {
-        $this->_genre=$genre;
+        $this->_genre=strtoupper($genre);
+    }
+
+    //Autres méthodes
+    public function toString()
+    {
+        $reponse = "Le nom est : \t".$this->_nom."\nLe prénom est : ".$this->_prenom." \nL'age est : \t". $this->_age."\nLe genre est : \t".$this->_genre."\n";
+        return $reponse;
+    }
+
+    public function equalsTo($obj)
+    {
+        if ($this->_nom==$obj->getNom() && $this->_prenom==$obj->getPrenom() && $this->_age==$obj->getAge() && $this->_genre==$obj->getGenre())
+        {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function compareTo($obj)
+    {
+        if(!($this->_nom==$obj->getNom() && $this->_prenom==$obj->getPrenom()))
+        {
+            if($this->_nom > $obj->getNom())
+            {
+                return 1;
+            }
+            else
+            {
+                if ($this->_prenom > $obj->getPrenom())
+                {
+                    return 1;
+                }
+                return -1;
+            }
+        }
+        return 0;
     }
 }
 
-$p1=new Personne ("Mayeux","Bruno",41,"H");
+$p1=new Personne ();
+$p2=new Personne ("Dupont","Toto",20,"H");
+//echo $p1->toString();
+//echo $p2->toString();
+echo $p1->compareTo($p2);
 var_dump($p1);
