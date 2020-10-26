027de54b868c49cfd38adfee62fd0676e41d41df
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-10-26 11:16:34 +0100

    26/10/2020 11:16:34,65

--- Bases de donnees/exemple.sql
+++ Bases de donnees/exemple.sql
@@ -14,10 +14,22 @@
 9 - SELECT CONCAT (`nom`," ",`prenom`) AS "Nom Employe",`salaire`,`nodep` from `employe`
     WHERE `titre`="représentant" AND `nodep`=35 AND `salaire`>20000 
 10- SELECT CONCAT (`nom`," ",`prenom`) AS "Nom Employe",`salaire`,`nodep` from `employe`
-    WHERE `titre`="représentant" AND `nodep`=35 AND `salaire`>20000 
+    WHERE `titre`="représentant" OR `titre`="président"
 11- SELECT CONCAT (`nom`," ",`prenom`) AS "Nom Employe",`salaire`,`nodep` from `employe`
     WHERE (`titre`="représentant" OR `titre`="secrétaire")  AND `nodep`=34
 12- SELECT CONCAT (`nom`," ",`prenom`) AS "Nom Employe",`salaire`,`nodep` from `employe`
-    WHERE `titre`="représentant" OR `titre`="secrétaire"  OR `nodep`=34
-13- SELECT CONCAT (`nom`," ",`prenom`) AS "Nom Employe",`salaire`,`nodep` from `employe`
+    WHERE `titre`="représentant" OR (`titre`="secrétaire"  AND `nodep`=34)
+13- SELECT `nom`,`salaire`,`nodep` from `employe`
     WHERE `salaire`>=20000 AND `salaire`<= 30000
+15- SELECT `nom` from `employe` 
+    WHERE `nom` LIKE "H%"
+16- SELECT `nom` from `employe`
+    WHERE `nom` LIKE "%n"
+17- SELECT `nom` from `employe`
+    WHERE `nom` LIKE "__u%"
+18- SELECT `salaire`,`nom` from `employe`
+    WHERE `nodep`=41 
+    ORDER BY `salaire`
+19- SELECT `salaire`,`nom` from `employe`
+    WHERE `nodep`=41 
+    ORDER BY `salaire` DESC
