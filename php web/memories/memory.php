683ba7e48d55bf4bae17d3ceb9a486fc05a1fda2
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-09 16:58:23 +0100

    09/11/2020 16:58:23,73

--- php web/memories/memory.php
+++ php web/memories/memory.php
@@ -16,28 +16,26 @@
         $photo[$a+1]=$num;
         $num++;
     }
-   
+    
+    echo'<div class="titre">Jeu de Memory</div>';
+    
     for($i=1;$i<5;$i++)
     {
         echo'<div class="espacehor"></div>
         <div class="ligne">
         <div class="vide"></div>';
-            for($j=1;$j<5;$j++)
-            {
-                /*do{*/
-                    $numphoto=rand(1,8);
-                    echo $numphoto;
-                    $index=array_search($numphoto,$photo);
-                    
-                /*}while($index==NULL);
-                echo $index;*/
-                unset($photo[$index]);
-                echo'<div class="case">
-                <img class="recto" src="Images/plage.jpg" alt="">
-                <img class="verso" src="Images/1.jpg" alt="">
-                </div>
-                <div class="vide"></div>';
-            }
+        
+        for($j=1;$j<5;$j++)
+        {
+            $index=array_rand($photo);
+            $numphoto=$photo[$index];
+            echo'<div class="case">
+            <img class="recto" src="Images/plage.jpg" alt="">
+            <img class="verso" src="Images/'.$numphoto.'.jpg" alt="">
+            </div>
+            <div class="vide"></div>';
+            unset($photo[$index]);
+        }
         echo'</div>
         <div class="vide"></div>';
     }
