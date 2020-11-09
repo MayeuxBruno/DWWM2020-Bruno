592f206c225e51d09ad02b84cc87678f99817d74
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-09 14:58:23 +0100

    09/11/2020 14:58:23,71

--- php web/memories/memory.php
+++ php web/memories/memory.php
@@ -4,14 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="memory.css">
 </head>
 <body>
-    <div class="ligne">
-        <div class="case">
-            <img class="recto" src="/imageplage.jpg" alt="">
-            <img class="verso" src="1.jpg" alt="">
-        </div>
-    </div>
+    
+    <?php
+    $photo=array();
+    for($a=0;$a<8;$a++){
+        
+    }
+    for($i=1;$i<5;$i++)
+    {
+        echo'<div class="espacehor"></div>
+        <div class="ligne">
+        <div class="vide"></div>';
+            for($j=1;$j<5;$j++)
+            {
+                $photo=rand(1,8);
+                echo'<div class="case">
+                <img class="recto" src="Images/plage.jpg" alt="">
+                <img class="verso" src="Images/'.$photo.'.jpg" alt="">
+                </div>
+                <div class="vide"></div>';
+            }
+        echo'</div>
+        <div class="vide"></div>';
+    }
+    ?> 
+    
 </body>
 </html>
