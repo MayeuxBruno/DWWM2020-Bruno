4b06a88a4510bceca313692c8e831d6a96842086
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-13 11:20:59 +0100

    13/11/2020 11:20:59,04

--- php web/classetest/test.php
+++ php web/classetest/test.php
@@ -4,9 +4,20 @@ include 'head.php';
 include 'header.php';
 include 'personne.php';
 
-echo'<div class="ligne'>';
-echo'<div class="personne">';
-    echo'<h1>'.$personne[0]->getNom().'</h1>';
+echo'<div class="ligne">';
+$cmp=0;
+foreach($personne as $elt)
+{
+    echo'<div class="personne">
+    <span>Nom : '.$elt->getNom().'</span>';
+    echo'</div>';
+    $cmp++;
+    if($cmp==3){
+        echo'</div>';
+        echo'<div class="ligne">';
+        $cmp=0;
+    }
+}
 echo'</div>';
 
 include 'footer.php';
