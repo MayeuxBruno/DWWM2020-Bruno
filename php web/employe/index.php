496c10c701177ff303bca24c8af3d0fd182603f1
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-16 14:27:02 +0100

    16/11/2020 14:27:02,81

--- php web/employe/index.php
+++ php web/employe/index.php
@@ -21,10 +21,17 @@ for($i=0;$i<count($e);$i++)
 echo'<div>';*/
 
 echo'<div class="colonne">
+     <div class="espaceh"></div>
      <div class="employe">
      <div></div>
-        <div></div>
-        </div>
+     <div class="bleuciel">
+        <div class="cellule">Agence</div>
+        <div class="cellule">Service</div>
+        <div class="cellule">Nom</div>
+        <div class="cellule">Prenom</div>
+     </div> 
+     <div></div>
+     </div>
      ';
 $compteur=0;
 
@@ -33,10 +40,13 @@ for($i=0;$i<count($e);$i++)
     echo'<div class="employe">
         <div></div>
         <div class="cache">'.$e[$i]->getIdEmploye().'</div>
+        <div class="vertFonce">
             <div class="cellule agence"><a href="detail.php?id='.$e[$i]->getIdEmploye().'">'.$e[$i]->getAgence()->getNom().'</a></div>
+            <div class="cellule agence"><a href="detail.php?id='.$e[$i]->getIdEmploye().'">'.$e[$i]->getService().'</a></div>
             <div class="cellule nom"><a href="detail.php?id='.$e[$i]->getIdEmploye().'">'.$e[$i]->getNom().'</a></div>
             <div class="cellule prenom"><a href="detail.php?id='.$e[$i]->getIdEmploye().'">'.$e[$i]->getPrenom().'</a></div>
-            <div></div>
+        </div>    
+        <div></div>
         </div>
         ';
 }
