36cc16882cc0ab472f0e36c099b60e6853277d49
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-16 12:27:02 +0100

    16/11/2020 12:27:02,79

--- php web/employe/detail.php
+++ php web/employe/detail.php
@@ -6,35 +6,67 @@ include "listeEmploye.php";
 
 $idRecherche = $_GET['id'];
 
-/*foreach ($e as $employe)
-{
-    if($employe->getIdEmploye()==$idRecherche)
-    {
-        echo'<div class="ligne">';
-
-        echo'<div class="personne colonne">
-        <div class="nom">'.$employe->getNom().' '.
-        $employe->getPrenom().'</div>
-        <div class="embauche">Embauche le : '.$employe->getDateEmbauche()->format('d-m-Y').'</div>
-        <div class="service">'.$employe->getService().'</div>
-        </div>';
-
-        echo '</div>';
-    }
-}*/
 foreach ($e as $employe)
 {
     if($employe->getIdEmploye()==$idRecherche)
     {
-        echo'<h2>'.$employe->getNom().' '.$employe->getPrenom().'</h2>';
+        echo'<div class="container colonne">
+              <div class="centre"><h3>'.$employe->getNom().' '.$employe->getPrenom().'</h3></div>';
         echo '<div class="information">
-              <ul>
-                <li> Embauché le :'..$employe->getDateEmbauche()->format('d-m-Y').'</li>
-                <li
-        <div class="service">'.$employe->getService().'</div>
+              <div></div>
+              <div class="titre centre"><h2>Informations</h2></div>
+              <div class="liste">
+                <ul>
+                    <li> Embauché le :'.$employe->getDateEmbauche()->format('d-m-Y').'</li>
+                    <li> Poste : '.$employe->getFonction().'</li>
+                    <li> Salaire : '.$employe->getSalaireAnnuel().' K€</li>
+                </ul>
+              </div>
+              <div></div>
         </div>';
 
-        echo '</div>';
+        echo '<div class="Agence">
+              <div></div>
+              <div class="titre centre"><h2>Poste</h2></div>
+                <div class="liste">
+                    <ul>
+                        <li>'.$employe->getAgence()->getNom().'</li>
+                        <li>'.$employe->getAgence()->getAdresse().'</li>
+                        <li>'.$employe->getAgence()->getCodePostal().' '.$employe->getAgence()->getVille().'</li>
+                    </ul>
+                </div>
+                <div></div>
+            </div>';
+
+        echo '<div class="Poste">
+              <div></div>
+              <div class="titre centre"><h2>Poste</h2></div>
+              <div class="liste">
+                <ul>
+                    <li>'.$employe->getService().'</li>
+                 </ul>
+              </div>
+              <div></div>
+        </div>';
+
+        if($employe->getEnfants()!=NULL)
+        {
+            echo '<div class="Enfant">
+                <div></div>    
+                <div class="titre centre"><h2>Enfant(s)</h2></div>
+                <div class="liste">
+                <ul>';
+                foreach($employe->getEnfants() as $enfant)
+                {
+                     echo'<li>'.$enfant->getNom().' '.$enfant->getPrenom().' '.$enfant->getAge().'</li>';
+                }
+            echo'</ul>
+            </div>
+            </div>';
+        }
+        echo '
+        <div></div>    
+        </div>';
     }
 }
 echo '<a href="index.php">Retour</a>';
