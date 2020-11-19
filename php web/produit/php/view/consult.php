703f4c2c7edfe3dfdd0283322e4150fdd8221916
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-19 12:40:25 +0100

    19/11/2020 12:40:24,92

--- php web/produit/php/view/consult.php
+++ php web/produit/php/view/consult.php
@@ -6,14 +6,31 @@ include "Head.php";
 include "Header.php";
 
 $idProduit=$_GET['id'];
+$produit=ProduitsManager::findById($idProduit);
 
-echo'<div claas="colonne">';
+     echo'<div class="colonne">
+     <div class="espacehor"></div>
+     <div class="ligne">
+     <div></div>
+     <div class="bouton blanc">Nom</div>
+     <div class="bouton vertclair"><input type="text" disabled="disabled" value="'.$produit->getLibelleProduit().'"/></div>
+     <div></div>
+     </div>
+     <div class="ligne">
+     <div></div>
+     <div class="bouton blanc">Prix</div>
+     <div class="bouton vertclair"><input type="text" disabled="disabled" value="'.$produit->getPrix()."€".'"/></div>
+     <div></div>
+     </div>
+     <div class="ligne">
+     <div></div>
+     <div class="bouton blanc">Date de peremption</div>
+     <div class="bouton vertclair"><input type="text" disabled="disabled" value="'.$produit->getDateDePeremption().'"/></div>
+     <div></div>
+     </div>
+     </div>
 
-
-
-
-echo'   </div>
-        <div class="espacehor"></div>
-        <div class="bouton vertclair">Retour</div>';
+     <div class="espacehor"></div>
+     <div class="bouton vertclair"><a href="../../index.php">Retour</a></div>';
 
 include "Footer.php";
