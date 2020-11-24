b63b760068bc3e4c811341577fe96cd39c721b56
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-11-24 09:51:14 +0100

    24/11/2020  9:51:14,16

---
+++ php web/test/PHP/VIEW/Head.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+
+function chargerClasse($classe)
+{
+    if (file_exists("php/controller/".$classe.".class.php"))
+    {
+        require "php/controller/".$classe.".class.php";
+    }
+    else if (file_exists("../controller/".$classe.".class.php"))
+    {
+        require "../controller/".$classe.".class.php";
+    }
+
+
+    if (file_exists("php/model/".$classe.".class.php"))
+    {
+        require "php/model/".$classe.".class.php";
+    }
+    else if (file_exists("../model/".$classe.".class.php"))
+    {
+        require "../model/".$classe.".class.php";
+    }
+}
+
+spl_autoload_register("chargerClasse");
+
+DbConnect::Init();
+
+echo(!empty($titre))?'<title>'.$titre.'</title>':'<title>Titre de la page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+
+if (file_exists("./css/style.css"))
+{
+    echo'<link rel="stylesheet" href="./css/style.css">';
+}
+else if (file_exists("../../css/style.css"))
+{
+    echo'<link rel="stylesheet" href="../../css/style.css">';
+}
+
+echo'</head>';
