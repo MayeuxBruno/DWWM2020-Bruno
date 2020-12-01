a1550249a5146f54c72c1607d975e0a98b79ed37
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-12-01 19:46:49 +0100

    01/12/2020 19:46:48,88

--- php web/projet connexion multilingues Crud/PHP/VIEW/ListeVehicule.php
+++ php web/projet connexion multilingues Crud/PHP/VIEW/ListeVehicule.php
@@ -1,9 +1,32 @@
 <?php
 
-$vehicules=Vehicules::getListe();
+//var_dump($_SESSION);
+$vehicules=VehiculesManager::getList();
 
+if ($_SESSION['utilisateur']->getRoleUser()==2)
+{
+    echo '<div class="demiespacehor"></div>';
+    echo '<div class="center"><button>Ajouter un véhicule</button></div>';
+    echo '<div class="demiespacehor"></div>';
+}
+else{
+    echo '<div class="espacehor"></div>';
+}
+echo'<div>';
+echo'<div></div>';
+echo '<div class="colonne">';
 foreach ($vehicules as $unvehicule)
 {
-    echo '<div class="colonne">
-        <div>'.
+        echo'<div>';
+        echo '<div class="center">'.$unvehicule->getNoparc().'</div>';
+        echo '<div class="center"><button>Détail</button></div>';
+        if ($_SESSION['utilisateur']->getRoleUser()==2)
+        {
+            echo '<div class="center"><button>Modifier</button></div>';
+            echo '<div class="center"><button>Supprimmer</button></div>';
+        }
+        echo '</div>';
 }
+echo '</div>';
+echo'<div></div>';
+echo '</div>';
