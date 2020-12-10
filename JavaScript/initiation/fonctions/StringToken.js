71152fa795747992c3813bd18fe51f288dd26e16
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-12-10 15:27:46 +0100

    10/12/2020 15:27:46,15

--- JavaScript/initiation/fonctions/StringToken.js
+++ JavaScript/initiation/fonctions/StringToken.js
@@ -1,17 +1,29 @@
 var chaine1;
 var chaine2;
 var n;
+
 function strtok(str1,str2,n)
 {
-    let i=0;
-    let l=0;
-    n=n-1;
-
+    let index=0;
+    let indexfin;
+    for(i=1;i<n;i++)
+    {
+        index=str1.indexOf(str2,index+1);
+    }
     
-    //document.write(str1.substr(l,3))
+    if((indexfin=str1.indexOf(str2,index+1))==-1)
+    {
+        document.write(str1.substr(index+1,str1.length-1));
+    }
+    else{
+        (index==0)?index=index:index=index+1;
+        document.write(str1.substr(index,((indexfin)-index)));
+    }
+    
+   
 }
 
-chaine1 ="robert;dupont,amiens,80000";
+chaine1 ="robert;dupont;amiens;80000";
 chaine2=";";
 n = parseInt(prompt("entrez le numéro du mot à extraire (de 1 à 4): "));
 strtok(chaine1,chaine2,n);
