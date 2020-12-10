b3c9603fcd4471d5360c2b39b2e493d1dccc74d7
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-12-10 14:27:46 +0100

    10/12/2020 14:27:46,12

--- JavaScript/initiation/fonctions/Menu.js
+++ JavaScript/initiation/fonctions/Menu.js
@@ -12,6 +12,24 @@ function TableMultiplication()
     }
 }
 
+function Somme()
+{
+    let somme=0;
+    let nbr,cpt=0;
+    do
+    {
+        nbr=parseInt(prompt("Entrez un nombre (0 pour arreter) : "));
+        if (nbr!=0)
+        {
+            somme+=nbr;
+            cpt++;
+        }
+    }while(nbr!=0);
+
+    document.write("La somme est de "+somme+"<br>");
+    document.write("La moyenne est de "+(somme/cpt));
+}
+
 function Voyelles()
 {
     let chaine;
@@ -31,6 +49,29 @@ function Voyelles()
     document.write("Le mot entré contient "+voyelles+" voyelles");
 }
 
+function CompteLettre()
+{
+    let phrase=prompt("Entrez une phrase : ").toLowerCase()
+    let lettre=prompt("Entrez la lettre à rechercher : ").toLowerCase()
+    let nb,l,i;
+    for(l=0;l<lettre.length;l++)
+    {
+        nb=0;
+        caractere=lettre.substr(l,1);
+        if (caractere>='a'&& caractere<='z') //Ignorer tout acarctère qui ne serait pas une lettre
+        {
+            for(i=0;i<phrase.length;i++)
+            {
+                if((phrase.substr(i,1))==caractere)
+                {
+                    nb++;
+                }
+            }
+            document.write("La lettre "+caractere+" apparait "+nb+" fois dans la phrase<br>");
+        }
+    }
+}
+
 var choix=prompt("1- Multiples\n2- Somme et moyenne\n3- Recherche du nombre de voyelles\n4- Recherche du nombre des caractères suivants\nEntrez votre choix");
 switch(choix)
 {
@@ -38,12 +79,13 @@ switch(choix)
         TableMultiplication();
         break;
     case "2":
-        
+        Somme();
         break;
     case "3":
         Voyelles();
         break;
     case "4":
+        CompteLettre();
         break;
         default:
             document.write("Choix incorrecte");
---
+++ JavaScript/initiation/fonctions/StringToken.js
@@ -0,0 +1,17 @@
+var chaine1;
+var chaine2;
+var n;
+function strtok(str1,str2,n)
+{
+    let i=0;
+    let l=0;
+    n=n-1;
+
+    
+    //document.write(str1.substr(l,3))
+}
+
+chaine1 ="robert;dupont,amiens,80000";
+chaine2=";";
+n = parseInt(prompt("entrez le numéro du mot à extraire (de 1 à 4): "));
+strtok(chaine1,chaine2,n);
