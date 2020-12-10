f17dcab5a3f60004061ebbb70dcc4719f9f935ed
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-12-10 13:27:50 +0100

    10/12/2020 13:27:50,36

--- JavaScript/initiation/fonctions/Menu.js
+++ JavaScript/initiation/fonctions/Menu.js
@@ -1,8 +1,9 @@
 function TableMultiplication()
 {
+    let i,nb;
     do
     {
-        var nb=parseInt(prompt("Entrez la table à afficher : "));
+        nb=parseInt(prompt("Entrez la table à afficher : "));
     }while(isNaN(nb)||nb<0);
 
     for(i=1;i<=10;i++)
@@ -11,6 +12,24 @@ function TableMultiplication()
     }
 }
 
+function Voyelles()
+{
+    let chaine;
+    let carac;
+    let voyelles=0;
+    chaine=prompt("Veuillez saisir un mot : ");
+    chaine=chaine.toLowerCase()
+
+    for (i=0;i<chaine.length;i++)
+    {
+        carac=chaine.substr(i,1);
+        if (carac=="a"||carac=="e"||carac=="i"||carac=="o"||carac=="u"||carac=="y")
+        {
+            voyelles+=1;
+        }
+    }
+    document.write("Le mot entré contient "+voyelles+" voyelles");
+}
 
 var choix=prompt("1- Multiples\n2- Somme et moyenne\n3- Recherche du nombre de voyelles\n4- Recherche du nombre des caractères suivants\nEntrez votre choix");
 switch(choix)
@@ -19,8 +38,10 @@ switch(choix)
         TableMultiplication();
         break;
     case "2":
+        
         break;
     case "3":
+        Voyelles();
         break;
     case "4":
         break;
