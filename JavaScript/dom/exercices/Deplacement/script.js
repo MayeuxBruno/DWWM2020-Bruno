c1cb219b475897478fab85fe7190d38068c89ce8
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2020-12-15 17:34:52 +0100

    15/12/2020 17:34:52,18

--- JavaScript/dom/exercices/Deplacement/script.js
+++ JavaScript/dom/exercices/Deplacement/script.js
@@ -1,11 +1,36 @@
 var carre=document.getElementById("carre");
 var btUp=document.getElementById("up");
+var btDown=document.getElementById("down");
+var btLeft=document.getElementById("left");
+var btRight=document.getElementById("right");
+var x=10;
+var y=10;
 
 function moveUp()
 {
-    var x=carre.style.top;
-    //carre.style.top=x+"em";
-    document.write(x);
+    x-=1;
+    carre.style.top=x+"em";
+}
+
+function moveDown()
+{
+    x+=1;
+    carre.style.top=x+"em";
+}
+
+function moveLeft()
+{
+    y-=1;
+    carre.style.left=y+"em";
+}
+
+function moveRight()
+{
+    y+=1;
+    carre.style.left=y+"em";
 }
 
 btUp.addEventListener("click",moveUp);
+btDown.addEventListener("click",moveDown);
+btLeft.addEventListener("click",moveLeft);
+btRight.addEventListener("click",moveRight);
