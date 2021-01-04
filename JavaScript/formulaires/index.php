c1d90014a287eef10129c5791e7bf4127c8a9a1d
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-01-04 17:25:22 +0100

    04/01/2021 17:25:22,09

--- JavaScript/formulaires/index.php
+++ JavaScript/formulaires/index.php
@@ -15,24 +15,52 @@
                 <form id="form" action="" method="POST">
                     <fieldset>
                         <legend>Vos Coordonnées</legend>
+                        <div class="champs">Les champs ( <span class="rouge">*</span> ) sont obligatoires</div>
                             <div class="champs">
                                 <div><label for="nom">Nom:</label></div>
-                                <div><input name="nom" type="text" required><span></span></div>
-                                
+                                <div><input name="nom" type="text" required><div></div></div>
+                                <div class="logo">
+                                    <div><img src="point.png" title="Entrez votre nom de famille"></div>
+                                    <div></div>
+                                </div>
+                                <div class="logo">
+                                    <img id="nomKo" src="croix.png">
+                                </div>
                             </div>
                             <div class="champs">
                                 <div><label for="prenom">Prenom:</label></div>
-                                <div><input name="prenom" type="text" required><span></span></div>
+                                <div><input name="prenom" type="text" required><div></div></div>
+                                <div class="logo">
+                                    <div><img src="point.png"></div>
+                                    <div></div>
+                                </div>
+                                <div class="logo">
+                                    <img id="prenomKo" src="croix.png">
+                                </div>
                             </div>
                             <div class="champs">
                                 <div><label for="cp">Code Postal:</label></div>
-                                <div><input name="cp" type="number"><span></span></div>
+                                <div><input name="cp" type="text" title="5 chiffres attendus"><div></div></div>
+                                <div class="logo">
+                                    <div><img src="point.png"></div>
+                                    <div></div>
+                                </div>
+                                <div class="logo">
+                                    <img id="cpKo" src="croix.png">
+                                </div>
                             </div>
                             <div class="champs">
                                 <div><label for="dateNaissance">Date de Naissance:</label></div>
-                                <div><input name="dateNaissance" type="date" required><span></span></div>
+                                <div><input name="dateNaissance" type="date" required><div></div></div>
+                                <div class="logo">
+                                    <div><img src="point.png"></div>
+                                    <div></div>
+                                </div>
+                                <div class="logo">
+                                    <img id="dateKo" src="croix.png">
+                                </div>
                             </div>
-                                <div class="champs centre"><input type="submit" value="Envoyer"></div>
+                                <div class="champs centre"><input type="submit" value="Envoyer" disabled></div>
                     </fieldset>
                 </form>
                 <div class="centre champs" id="erreur"></div>
--- JavaScript/formulaires/script.js
+++ JavaScript/formulaires/script.js
@@ -4,35 +4,103 @@ var inputs=document.getElementsByTagName("input");
 var nom=inputs[0];
 var prenom=inputs[1];
 var cp=inputs[2];
+var dateNaissance=inputs[3];
+var nomOk=document.getElementById("nomKo");
+var prenomOk=document.getElementById("prenomKo");
+var cpOk=document.getElementById("cpKo");
+var dateOk=document.getElementById("dateKo");
 
 nom.addEventListener("keyup",function(){
     erreur.textContent="";
     var chaine=nom.value;
-    if(!isNaN(chaine[chaine.length-1]))
+    var code=chaine.charCodeAt(chaine.length-1)
+    console.log(code);
+    if(!((code<97||code>122)^(code<64||code>90)))
     {
-        nom.value = nom.value.slice(0,-1);
-        erreur.textContent="Le nom ne doit pas comporter de chiffres";
+        if(code!=45&&code!=39&&code!=32)
+        {
+            nomOk.setAttribute("src","croix.png");
+            nom.value = nom.value.slice(0,-1);
+            erreur.textContent="Ce caractère n'est pas accepte";
+        }
+        else{
+            nomOk.setAttribute("src","coche.png");    
+        }
+    }
+    else{
+        nomOk.setAttribute("src","coche.png");
+    }
+    if(chaine.length==0)
+    {
+        nomOk.setAttribute("src","croix.png");
     }
 });
 
 prenom.addEventListener("keyup",function(){
     erreur.textContent="";
     var chaine=prenom.value;
-    if(!isNaN(chaine[chaine.length-1]))
+    var code=chaine.charCodeAt(chaine.length-1)
+    console.log(code);
+    if(!((code<97||code>188)^(code<64||code>90)))
     {
-        prenom.value = prenom.value.slice(0,-1);
-        erreur.textContent="Le prénom de doit pas comporter de chiffres";
+        if(code!=45&&code!=39&&code!=32)
+        {
+            prenomOk.setAttribute("src","croix.png");
+            prenom.value = prenom.value.slice(0,-1);
+            erreur.textContent="Ce caractère n'est pas accepte";
+        }
+        else{
+            prenomOk.setAttribute("src","coche.png");    
+        }
+    }
+    else{
+        prenomOk.setAttribute("src","coche.png");
+    }
+    if(chaine.length==0)
+    {
+        prenomOk.setAttribute("src","croix.png");
     }
 });
- cp.addEventListener("keyup",function(){
+
+cp.addEventListener("keyup",function(){
     erreur.textContent="";
+    var chaine=cp.value;
+    console.log(cp.value.length);
      if(cp.value.length<5)
      {
+         if (isNaN(chaine[chaine.length-1]))
+         {
+            cp.value = cp.value.slice(0,-1);
+         }
         erreur.textContent="La code postal doit comporter 5 chiffres";
+        cpOk.setAttribute("src","croix.png");
      }
      else{
         if(cp.value.length>5){
             cp.value = cp.value.slice(0,-1);
         }
+
      }
- });
+     if(cp.value.length==5)
+     {
+        cpOk.setAttribute("src","coche.png");
+     }
+});
+
+dateNaissance.addEventListener("change",function(){
+    erreur.textContent="";
+	var date = dateNaissance.value;
+    var aaaa = date.substring(0,4);
+    var mm = date.substring(5,7);
+    var jj = date.substring(8,10);
+    var dates = new Date(aaaa,mm-1,jj);
+    var dateActuelle=new Date();
+    if (dates>dateActuelle)
+    {
+        erreur.textContent="La date de naissance est incorrecte";
+        dateOk.setAttribute("src","croix.png");
+    } 
+    else{
+        dateOk.setAttribute("src","coche.png");
+    } 
+});
