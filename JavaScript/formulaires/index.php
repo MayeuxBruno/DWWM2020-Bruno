aafdbdd5c4d21224699d25818dd2b084a2fbb09f
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-01-06 17:25:21 +0100

    06/01/2021 17:25:21,56

--- JavaScript/formulaires/index.php
+++ JavaScript/formulaires/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Titre</title>
 </head>
@@ -18,9 +19,9 @@
                         <div class="champs">Les champs ( <span class="rouge">*</span> ) sont obligatoires</div>
                             <div class="champs">
                                 <div><label for="nom">Nom:</label></div>
-                                <div><input class="checkinput" name="nom" type="text" pattern="[a-zA-Z '-]{3,}" required><div></div></div>
+                                <div><input class="checkinput" title="Entrez votre nom de Famille 3 lettres minimum" name="nom" type="text" pattern="[a-zA-Z '-éèàâô]{3,}" required><div></div></div>
                                 <div class="infobulle">
-                                    <div class="infobulle-texte" id="aide-nom">Entrez ici votre nom de famille</div>
+                                    <div class="infobulle-text"></div>
                                     <div><img src="point.png" class="infobulle-icone"></div>
                                 </div>
                                 <div class="logo"></div>
@@ -31,9 +32,9 @@
                             </div>
                             <div class="champs">
                                 <div><label for="prenom">Prenom:</label></div>
-                                <div><input class="checkinput" name="prenom" type="text" pattern="[a-zA-Z '-]{3,}" required><div></div></div>
+                                <div><input class="checkinput" title="Entrez votre prénom 3 lettres minimum" name="prenom" type="text" pattern="[a-zA-Z '-éèàâô]{3,}" required><div></div></div>
                                 <div class="infobulle">
-                                    <div class="infobulle-texte" id="aide-nom">Entrez ici votre prénom</div>
+                                <div class="infobulle-text"></div>
                                     <div><img src="point.png" class="infobulle-icone"></div>
                                     <div></div>
                                 </div>
@@ -46,9 +47,9 @@
                             <div class="champs">
                                 <div><label for="nom">Mot de Passe :</label></div>
                                 <div class="input_icon"><i id="oeil" class="far fa-eye"></i></div>
-                                <div><input class="checkinput" name="mdp" type="password" pattern="[a-zA-Z '-]{3,}" required><div></div></div>
+                                <div><input class="checkinput" title="Entrez votre mot de passe 8 caractères minimum" name="mdp" type="password" pattern="[a-zA-Z0-9]{8,}" required><div></div></div>
                                 <div class="infobulle">
-                                    <div class="infobulle-texte" id="aide-nom">Entrez ici votre nom de famille</div>
+                                    <div class="infobulle-text"></div>
                                     <div><img src="point.png" class="infobulle-icone"></div>
                                     <div></div>
                                 </div>
@@ -60,9 +61,9 @@
                             </div>
                             <div class="champs">
                                 <div><label for="cp">Code Postal:</label></div>
-                                <div><input class="checkinput" name="cp" type="text" pattern="\d{5}" maxlength="5" required><div></div></div>
+                                <div><input class="checkinput" title="Entrez votre code postal sur 5 chiffres" name="cp" type="text" pattern="\d{5}" maxlength="5" required><div></div></div>
                                 <div class="infobulle">
-                                    <div class="infobulle-texte" id="aide-nom">Entrez ici votre code postal sur 5 chiffres</div>
+                                    <div class="infobulle-text"></div>
                                     <div><img src="point.png" class="infobulle-icone"></div>
                                     <div></div>
                                 </div>
@@ -74,19 +75,19 @@
                             </div>
                             <div class="champs">
                                 <div><label for="dateNaissance">Date de Naissance:</label></div>
-                                <div><input class="checkinput" name="dateNaissance" type="date" pattern="[0123][0-9]{1}[/-]{1}[01][0-9]{1}[/-][0-9]{4}" required><div></div></div>
+                                <div><input class="checkinput" title="Entrez votre date de naissance" id="dateNaissance" name="dateNaissance" type="date" pattern="[0123][0-9]{1}[/-]{1}[01][0-9]{1}[/-][0-9]{4}" required><div></div></div>
                                 <div class="infobulle">
-                                    <div class="infobulle-texte" id="aide-nom">Entrez ici votre date de naissance</div>
+                                    <div class="infobulle-text"></div>
                                     <div><img src="point.png" class="infobulle-icone"></div>
                                     <div></div>
                                 </div>
                                 <div class="logo"></div>
                                 <div class="logo">
-                                    <img id="dateKo" src="croix.png">
+                                    <img class="voyant" src="croix.png">
                                 </div>
                                 <div class="message"></div>
                             </div>
-                                <div class="champs centre"><input id="submit" type="submit" value="Envoyer"></div>
+                                <div class="champs centre"><input id="submit" type="submit" value="Envoyer" disabled></div>
                     </fieldset>
                 </form>
                 <div class="centre champs" id="erreur"></div>
@@ -94,7 +95,6 @@
             <div></div>
         </div>
     </div>
-    <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
 </html>	
--- JavaScript/formulaires/script.js
+++ JavaScript/formulaires/script.js
@@ -1,112 +1,104 @@
 var submit=document.getElementById("submit");
-var erreur=document.getElementById("erreur");
-var inputs=document.getElementsByTagName("input");
+var dateNaissance=document.getElementById("dateNaissance");
 var infobulle=document.getElementsByClassName("infobulle-icone");
 var texteInfobulle=document.getElementsByClassName("infobulle-texte");
-var nom=inputs[0];
-var prenom=inputs[1];
-var mdp=inputs[2];
-var cp=inputs[3];
-var dateNaissance=inputs[4];
-var nomOk=document.getElementById("nomKo");
-var mdpOk=document.getElementById("mdpKo");
-var prenomOk=document.getElementById("prenomKo");
-var cpOk=document.getElementById("cpKo");
-var dateOk=document.getElementById("dateKo");
 var oeil=document.getElementById("oeil");
-var checkinput=document.getElementsByClassName("checkinput");
+var inputs=document.getElementsByClassName("checkinput");
 var check={"nom":false,"prenom":false,"mdp":false,"cp":false,"dateNaissance":false};
 
 //Gestion des infos-bulles
 for(let i=0;i<infobulle.length;i++)
 {
-    infobulle[i].addEventListener("mouseover",function(){
-        texteInfobulle[i].style.display="block";
+    infobulle[i].addEventListener("mouseover",function(event){
+        event.target.parentNode.parentNode.getElementsByClassName("infobulle-text")[0].innerHTML=event.target.parentNode.parentNode.parentNode.querySelector("input").getAttribute("title");
     });
-    infobulle[i].addEventListener("mouseout",function(){
-        texteInfobulle[i].style.display="none";
+    infobulle[i].addEventListener("mouseout",function(event){
+        event.target.parentNode.parentNode.getElementsByClassName("infobulle-text")[0].innerHTML="";
     });
 }
-nom.addEventListener("keyup",verif);
-prenom.addEventListener("keyup",verif);
-cp.addEventListener("keyup",verif);
-mdp.addEventListener("keyup",verif);
+
+for(let i=0;i<inputs.length;i++)
+{
+    inputs[i].addEventListener("input",verif);
+}
 
 function verif(event)
 {
-    erreur.textContent="";
     var moninput=event.target;
-    var parent=(moninput.parentNode).parentNode;
-    var voyant=parent.getElementsByClassName("voyant")[0];
+    var voyant=moninput.parentNode.parentNode.getElementsByClassName("voyant")[0];
+    
     var nom=moninput.getAttribute("name");
-    //console.log(nom);
     
     if(moninput.checkValidity())
     {
         voyant.setAttribute("src","coche.png");
+        message(moninput,true);
         check[nom]=true;
     }
     else{
         voyant.setAttribute("src","croix.png");
+        message(moninput,false);
         check[nom]=false;
     }
     console.log(check);
     checkResult();
 }
 
-dateNaissance.addEventListener("change",function(){
-    erreur.textContent="";
+function message(input,value)
+{
+    var message=input.parentNode.parentNode.getElementsByClassName("message")[0];
+    switch(value)
+    {
+        case true:
+            message.innerHTML="Saisie valide";
+            break;
+        
+        case false:
+            message.innerHTML="Saisie invalide";
+            break;
+    }
+}
+dateNaissance.addEventListener("input",function(event){
+    var voyant=event.target.parentNode.parentNode.getElementsByClassName("voyant")[0];
     var date = dateNaissance.value;
-    if(checkinput[4].checkValidity()){
-        var aaaa = date.substring(0,4);
-        var mm = date.substring(5,7);
-        var jj = date.substring(8,10);
-        var dates = new Date(aaaa,mm-1,jj);
-        var dateActuelle=new Date();
-        if (dates>dateActuelle)
-        {
-            erreur.textContent="La date de naissance est incorrecte";
-            dateOk.setAttribute("src","croix.png");
-            check["dateNaissance"]=false; 
-        } 
-        else{
-            dateOk.setAttribute("src","coche.png");
-            check["dateNaissance"]=true; 
-        } 
-    }
-    else{
-         dateOk.setAttribute("src","croix.png");
-        check["dateNaissance"]=false;
-    }
+    var aaaa = date.substring(0,4);
+    var mm = date.substring(5,7);
+    var jj = date.substring(8,10);
+    var dates = new Date(aaaa,mm-1,jj);
+    var dateActuelle=new Date();
+    if (dates>dateActuelle)
+    {
+        voyant.setAttribute("src","croix.png");
+        check["dateNaissance"]=false; 
+    } 
+    else
+    {
+        voyant.setAttribute("src","coche.png");
+        check["dateNaissance"]=true; 
+    } 
+    
     checkResult();
 });
 
 function checkResult()
 {
     let erreur=false;
+    submit.disabled=false; 
     for (var i in check)
-    {
-        console.logcheck[i];
-    }
-    /*let i=0;
-    while(i<check.length&&erreur==false)
     {
         if(check[i]==false)
         {
             erreur=true;
-            submit.disabled=true;
         }
-        i++;
     }
-    if(erreur==false)
+    if(erreur==true)
     {
-        submit.disabled=false;
-    }*/
-    
+        submit.disabled=true;
+    }
 }
-oeil.addEventListener("mousedown",function(){
-    mdp.setAttribute("type","text");
+oeil.addEventListener("mousedown",function(event){
+    event.target.parentNode.parentNode.querySelector("input").setAttribute("type","text");
 })
-oeil.addEventListener("mouseup",function(){
-    mdp.setAttribute("type","password");
+oeil.addEventListener("mouseup",function(event){
+    event.target.parentNode.parentNode.querySelector("input").setAttribute("type","password");
 })
