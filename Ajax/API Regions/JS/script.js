2fdc534f9bae281063cee51b2dd5ca3205c0d979
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-01-12 12:25:01 +0100

    12/01/2021 12:25:01,64

--- Ajax/API Regions/JS/script.js
+++ Ajax/API Regions/JS/script.js
@@ -1,7 +1,8 @@
+var contenu=document.getElementById("contenu");
 const requ = new XMLHttpRequest();
 const requ1 = new XMLHttpRequest();
 
-requ.open('GET', '/Ajax/API Regions/Php/Model/ListeRegion.php', true);
+requ.open('GET', '/Ajax/API Regions/Php/Model/ListeRegion.php', true)
 requ.send(null);
 requ.onreadystatechange = function(event) {
     // XMLHttpRequest.DONE === 4
@@ -9,7 +10,7 @@ requ.onreadystatechange = function(event) {
         if (this.status === 200) {
             console.log("Réponse reçue: %s", this.responseText);
             reponse=JSON.parse(this.responseText);
-            genereSelect(reponse);
+            GenereSelect(reponse);
         } else {
             console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
         }
@@ -32,9 +33,11 @@ requ1.onreadystatechange = function(event) {
 
 function GenereSelect($tableau)
 {
-    ligne = document.createElement("tr");
-                ligne.setAttribute("class", "crudLigne");
-                colonne1 = document.createElement("td");
-                colonne1.setAttribute("class", "crudColonne");
-                ligne.appendChild(colonne1);
+            select = document.createElement("select");
+            select.id="selectRegion";
+            option = document.createElement("option1");
+            option.setAttribute("value", "test");
+            select.appendChild(option);
+            contenu.appendChild(select);
+            
 }
