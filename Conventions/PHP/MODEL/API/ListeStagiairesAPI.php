f2394302c77123d1ddb9065994412771e8b04729
MayeuxBruno <mayeux.bruno.mb@gmail.com>
2021-01-19 21:36:36 +0100

    19/01/2021 21:36:36,04

--- Conventions/PHP/MODEL/API/ListeStagiairesAPI.php
+++ Conventions/PHP/MODEL/API/ListeStagiairesAPI.php
@@ -2,10 +2,8 @@
 $idSession=$_POST['idSession'];
 $lesStagiaires=StagiaireFormationManager::getListBySession($idSession,false);
 $lesPeriodes=PeriodesStagesManager::getListBySession($idSession,false);
-foreach($lesStagiaires as $unStagiaire)
-{
-    $stage=StagesManager::getByStagiaire($unStagiaire->,$idPeriode)
-}
-die(var_dump($lesPeriodes));
+//$stage[]=StagesManager::getByStagiaire($lesStagiaires[0]->getIdStagiaire(),$lesPeriodes[0]->getIdPeriode());  
+//die(var_dump($lesPeriodes[0]->getIdPeriode()));
+die(var_dump(StagesManager::getByStagiaire(25,/*,$lesPeriodes[0]->getIdPeriode()*/3)));
 //echo json_encode(StagiaireFormationManager::getListBySession($idSession,true));
 ?>
